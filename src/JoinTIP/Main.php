48eab93d83088c341e1a44cc01267010545d38d0
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-08 16:53:58 -0400

    Create Main.php

---
+++ src/JoinTIP/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JoinTIP;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+
+  public function onEnable(){
+    $this->saveDefaultConfig();
+    $cfg = $this->getConfig();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger->info(TextFormat::GOLD."Join" . TextFormat::BLUE . "TIP" . TextFormat::GREEN . " Has Been Enabled!");
+    $this->getLogger->info(TextFormat:::BLUE."Created By" . TextFormat::BOLD . " " . TextFormat::LIGHT_PURPLE . "LegacyDEVS");
+    
+		
+    }
+    
+    public function onJoinEvent(PlayerJoinEvent $event){
+      $msg = $cfg->get("Message");
+      $p = $event->getPlayer();
+      $player = $p->getName();
+      $p->sendTip($msg);
+      
+      }
+      
+    }
+    
