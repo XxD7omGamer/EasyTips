415c5dfb90d08588f972414c851adac4cc9a1cf4
Bulk Developer <jaydenpatrick1110@gmail.com>
2015-07-08 21:35:31 -0400

    Update Main.php

--- src/EasyTips/Main.php
+++ src/EasyTips/Main.php
@@ -18,7 +18,7 @@ class Main extends PluginBase implements Listener{
     $config = $this->getConfig();
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getLogger()->info(TextFormat::GOLD."Join" . TextFormat::BLUE . "TIP" . TextFormat::GREEN . " Has Been Enabled!");
-    $this->getLogger()->info(TextFormat::BLUE."Created By" . TextFormat::BOLD . " " . TextFormat::LIGHT_PURPLE . "LegacyDEVS");
+    $this->getLogger()->info(TextFormat::BLUE."Created By" . TextFormat::BOLD . " " . TextFormat::LIGHT_PURPLE . "ItzBulkDev");
     
 		
     }
